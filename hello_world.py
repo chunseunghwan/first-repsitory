7992dcacedf2353aaa33f6e6db86b1ef92d7a8e5
chunseunghwan <t91071153@gmail.com>
2023-09-13 15:37:16 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,9 @@ def print_hello():
 def my_impormation():
     print("이름: 천승환\n","생년 월일:2004/03/05\n","재학 중인 학교:금오공대")
 
+a=int(input("값 입력:"))
+print(type(a))
+
 if __name__ == '__main__':
     print_hello()
     
